3616838870f1f1ed32cb9512f40462bf8c3b33a5
ridhampokiya2110 <ridhampokiya10@gmail.com>
2026-07-22 22:48:55 +0530

    sdf

--- app/layout.tsx
+++ app/layout.tsx
@@ -370,7 +370,7 @@ export default function RootLayout({
                 <GlobalSEO />
       </head>
       <body className={inter.variable + " " + outfit.variable + " font-sans bg-[#FDFCF8] text-[#1B1716] antialiased selection:bg-cherry/20 selection:text-cherry"} suppressHydrationWarning>
-        <div className="w-full overflow-hidden flex flex-col min-h-screen">
+        <div className="w-full max-w-full overflow-x-hidden flex flex-col min-h-screen">
           <Providers>
             <PwaRegistry />
             <NextTopLoader color="#E44234" showSpinner={false} height={3} shadow="0 0 10px #E44234,0 0 5px #E44234" />
